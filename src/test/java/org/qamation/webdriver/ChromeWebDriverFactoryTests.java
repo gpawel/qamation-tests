92937a33df726ca3fc528160a97321200cc116b7
gpawel <gpawel@gmail.com>
2022-02-21 19:48:27 -0500

    completed tests for the local driver factory methods

--- src/test/java/org/qamation/webdriver/ChromeWebDriverFactoryTests.java
+++ src/test/java/org/qamation/webdriver/ChromeWebDriverFactoryTests.java
@@ -1,21 +1,71 @@
 package org.qamation.webdriver;
 
+import org.junit.After;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.qamation.commons.config.Config;
 import org.qamation.commons.webdriver.ChromeDriverFactory;
 
-public class ChromeWebDriverFactoryTests {
+import java.time.Duration;
+import java.util.Objects;
 
+public class ChromeWebDriverFactoryTests {
+    private WebDriver driver = null;
+    private By q = By.cssSelector("input[name='q']");
+
+    @BeforeClass
+    public static void setUp() {
+        System.setProperty("env","test");
+    }
+
+    @After
+    public void tearDown() {
+        //if (Objects.nonNull(driver)) driver.quit();
+    }
     @Test
     public void createLocalChromeDriver() {
-        WebDriver driver = ChromeDriverFactory.createLocalChromeDriver();
+        driver = ChromeDriverFactory.createLocalChromeDriver();
+        testDriverOnGoogle(driver);
+
+    }
+
+    @Test
+    public void createLocalChromeDriverUsingExecPath() {
+        driver = ChromeDriverFactory.createLocalChromeDriver(Config.getConfig().getChromeDriverPath());
+        testDriverOnGoogle(driver);
+    }
+
+    @Test
+    public void createLocalChromeDriverUsingOptions() {
+        ChromeOptions options = new ChromeOptions();
+        driver = ChromeDriverFactory.createLocalChromeDriver(options);
+        testDriverOnGoogle(driver);
+    }
+
+    @Test
+    public void createLocalChromeDriverUsingExecPathAndOptions() {
+        ChromeOptions options = ChromeDriverFactory.getChromeOptions();
+        driver = ChromeDriverFactory.createLocalChromeDriver(Config.getConfig().getChromeDriverPath(),options);
+        testDriverOnGoogle(driver);
+    }
+
+    private void waitForQ(WebDriver driver) {
+        Duration duration = Duration.ofSeconds(Config.getConfig().getPageLoadTimeOutMills()/1000);
+        Wait wait = new WebDriverWait(driver, duration);
+        wait.until(ExpectedConditions.elementToBeClickable(q));
+    }
+
+    private void testDriverOnGoogle(WebDriver driver) {
         driver.get("http://google.com");
-        try {
-            Thread.sleep(5000);
-        } catch (InterruptedException e) {
-            e.printStackTrace();
-        }
-        driver.quit();
+        driver.manage().window().fullscreen();
+        waitForQ(driver);
     }
 
 
